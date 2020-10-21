76decd83af3a6b3a9d6519329243f6ae3c4af1ca
petvi <petrasvilkelis@gmail.com>
2020-10-21 13:51:13 +0200

    Finish  All Components

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+import {
+  Container,
+  Spinner,
+} from "react-bootstrap";
+
+import Job from "./Job";
+import JobsPagination from "./JobsPagination";
+import SearchForm from "./SearchForm";
+
+import useFetchJobs from "./useFetchJobs";
+
+function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+
+  const {
+    jobs,
+    loading,
+    error,
+    hasNextPage,
+  } = useFetchJobs(params, page);
+
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  }
+
+  return (
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs </h1>
+      <SearchForm
+        params={params}
+        onParamChange={handleParamChange}
+      />
+      <JobsPagination
+        page={page}
+        setPage={setPage}
+        hasNextPage={hasNextPage}
+      />
+      {loading && (
+        <Spinner
+          animation="border"
+          role="status"
+          className="mb-2"
+        >
+          <span className="sr-only">
+            Loading...
+          </span>
+        </Spinner>
+      )}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
+      })}
+      <JobsPagination
+        page={page}
+        setPage={setPage}
+        hasNextPage={hasNextPage}
+      />
+      <p>Petras Vilkelis &copy; 2020</p>
+    </Container>
+  );
+}
+
+export default App;
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -9,6 +9,7 @@ const ACTIONS = {
   MAKE_REQUEST: "make-request",
   GET_DATA: "get-data",
   ERROR: "error",
+  UPDATE_HAS_NEXT_PAGE: "update-has-next-page",
 };
 
 function reducer(state, action) {
@@ -22,7 +23,7 @@ function reducer(state, action) {
       return {
         ...state,
         loading: false,
-        jobs: [action.payload.jobs],
+        jobs: [...action.payload.jobs],
       };
     case ACTIONS.ERROR:
       return {
@@ -30,6 +31,11 @@ function reducer(state, action) {
         loading: false,
         error: action.payload.error,
         jobs: [],
+      };
+    case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+      return {
+        ...state,
+        hasNextPage: action.payload.hasNextPage,
       };
     default:
       return state;
@@ -46,9 +52,11 @@ export default function useFetchJobs(
   });
 
   useEffect(() => {
+    const cancelToken1 = axios.CancelToken.source();
     dispatch({ type: ACTIONS.MAKE_REQUEST });
     axios
       .get(BASE_URL, {
+        cancelToken: cancelToken1.token,
         params: {
           markdown: true,
           page,
@@ -62,11 +70,43 @@ export default function useFetchJobs(
         });
       })
       .catch((err) => {
+        if (axios.isCancel(err)) return;
         dispatch({
           type: ACTIONS.ERROR,
           payload: { error: err },
         });
       });
+
+    const cancelToken2 = axios.CancelToken.source();
+    axios
+      .get(BASE_URL, {
+        cancelToken: cancelToken2.token,
+        params: {
+          markdown: true,
+          page: page + 1,
+          ...params,
+        },
+      })
+      .then((res) => {
+        dispatch({
+          type: ACTIONS.UPDATE_HAS_NEXT_PAGE,
+          payload: {
+            hasNextPage: res.data.length !== 0,
+          },
+        });
+      })
+      .catch((err) => {
+        if (axios.isCancel(err)) return;
+        dispatch({
+          type: ACTIONS.ERROR,
+          payload: { error: err },
+        });
+      });
+
+    return () => {
+      cancelToken1.cancel();
+      cancelToken2.cancel();
+    };
   }, [params, page]);
 
   return state;
