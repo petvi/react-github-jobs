44ed19f3e88b0146f84b2cd11c79910c7d8ad48d
petvi <petrasvilkelis@gmail.com>
2021-03-08 15:52:09 +0100

    Fix url

--- src/hooks/useFetchJobs.js
+++ src/hooks/useFetchJobs.js
@@ -1,16 +1,16 @@
-import { useReducer, useEffect } from "react";
+import { useReducer, useEffect } from 'react'
 
-import axios from "axios";
+import axios from 'axios'
 
 const BASE_URL =
-  "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
+  'https://secret-ocean-49799.herokuapp.com/https://jobs.github.com/positions.json'
 
 const ACTIONS = {
-  MAKE_REQUEST: "make-request",
-  GET_DATA: "get-data",
-  ERROR: "error",
-  UPDATE_HAS_NEXT_PAGE: "update-has-next-page",
-};
+  MAKE_REQUEST: 'make-request',
+  GET_DATA: 'get-data',
+  ERROR: 'error',
+  UPDATE_HAS_NEXT_PAGE: 'update-has-next-page',
+}
 
 function reducer(state, action) {
   switch (action.type) {
@@ -18,42 +18,39 @@ function reducer(state, action) {
       return {
         loading: true,
         jobs: [],
-      };
+      }
     case ACTIONS.GET_DATA:
       return {
         ...state,
         loading: false,
         jobs: [...action.payload.jobs],
-      };
+      }
     case ACTIONS.ERROR:
       return {
         ...state,
         loading: false,
         error: action.payload.error,
         jobs: [],
-      };
+      }
     case ACTIONS.UPDATE_HAS_NEXT_PAGE:
       return {
         ...state,
         hasNextPage: action.payload.hasNextPage,
-      };
+      }
     default:
-      return state;
+      return state
   }
 }
 
-export default function useFetchJobs(
-  params,
-  page
-) {
+export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, {
     jobs: [],
     loading: true,
-  });
+  })
 
   useEffect(() => {
-    const cancelToken1 = axios.CancelToken.source();
-    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    const cancelToken1 = axios.CancelToken.source()
+    dispatch({ type: ACTIONS.MAKE_REQUEST })
     axios
       .get(BASE_URL, {
         cancelToken: cancelToken1.token,
@@ -63,21 +60,21 @@ export default function useFetchJobs(
           ...params,
         },
       })
-      .then((res) => {
+      .then(res => {
         dispatch({
           type: ACTIONS.GET_DATA,
           payload: { jobs: res.data },
-        });
+        })
       })
-      .catch((err) => {
-        if (axios.isCancel(err)) return;
+      .catch(err => {
+        if (axios.isCancel(err)) return
         dispatch({
           type: ACTIONS.ERROR,
           payload: { error: err },
-        });
-      });
+        })
+      })
 
-    const cancelToken2 = axios.CancelToken.source();
+    const cancelToken2 = axios.CancelToken.source()
     axios
       .get(BASE_URL, {
         cancelToken: cancelToken2.token,
@@ -87,27 +84,27 @@ export default function useFetchJobs(
           ...params,
         },
       })
-      .then((res) => {
+      .then(res => {
         dispatch({
           type: ACTIONS.UPDATE_HAS_NEXT_PAGE,
           payload: {
             hasNextPage: res.data.length !== 0,
           },
-        });
+        })
       })
-      .catch((err) => {
-        if (axios.isCancel(err)) return;
+      .catch(err => {
+        if (axios.isCancel(err)) return
         dispatch({
           type: ACTIONS.ERROR,
           payload: { error: err },
-        });
-      });
+        })
+      })
 
     return () => {
-      cancelToken1.cancel();
-      cancelToken2.cancel();
-    };
-  }, [params, page]);
+      cancelToken1.cancel()
+      cancelToken2.cancel()
+    }
+  }, [params, page])
 
-  return state;
+  return state
 }
